3db6924c19cb64c3f52120d6deee90b23866ced7
aaronschweig <aaron.schweig@gmail.com>
2024-07-24 17:05:32 +0200

    chore: review suggegstions

--- api/v1alpha1/service.go
+++ api/v1alpha1/service.go
@@ -30,33 +30,43 @@ func NewService(client client.Client, rootNamespace string) *service {
 	}
 }
 
-func (s *service) GetFirstLevelAccountForAccount(ctx context.Context, accountKey client.ObjectKey) (*Account, error) {
-	return s.GetAccountForNamespace(ctx, accountKey.Namespace)
-}
-
-func (s *service) GetFirstLevelAccountForNamespace(ctx context.Context, namespace string) (*Account, error) {
+func (s *service) getAccountOwnerAndNamespaceForNamespace(ctx context.Context, namespace string) (string, string, error) {
 	var ns corev1.Namespace
 	err := s.client.Get(ctx, client.ObjectKey{Name: namespace}, &ns)
 	if err != nil {
-		return nil, err
+		return "", "", err
 	}
 
 	if ns.Labels == nil {
-		return nil, errors.New("namespace does not have a label and therefore no connected account")
+		return "", "", errors.New("namespace does not have a label and therefore no connected account")
 	}
 
 	accountNamespace, ok := ns.Labels[NamespaceAccountOwnerNamespaceLabel]
 	if !ok || accountNamespace == "" {
-		return nil, errors.New("namespace does not have an account-owner-namespace label and therefore no connected account")
-	}
-
-	if s.rootNamespace != accountNamespace {
-		return s.GetFirstLevelAccountForNamespace(ctx, accountNamespace)
+		return "", "", errors.New("namespace does not have an account-owner-namespace label and therefore no connected account")
 	}
 
 	accountName, ok := ns.Labels[NamespaceAccountOwnerLabel]
 	if !ok || accountName == "" {
-		return nil, errors.New("namespace does not have an account-owner label and therefore no connected account")
+		return "", "", errors.New("namespace does not have an account-owner label and therefore no connected account")
+	}
+
+	return accountName, accountNamespace, nil
+}
+
+func (s *service) GetFirstLevelAccountForAccount(ctx context.Context, accountKey client.ObjectKey) (*Account, error) {
+	return s.GetFirstLevelAccountForNamespace(ctx, accountKey.Namespace)
+}
+
+func (s *service) GetFirstLevelAccountForNamespace(ctx context.Context, namespace string) (*Account, error) {
+
+	accountName, accountNamespace, err := s.getAccountOwnerAndNamespaceForNamespace(ctx, namespace)
+	if err != nil {
+		return nil, err
+	}
+
+	if s.rootNamespace != accountNamespace {
+		return s.GetFirstLevelAccountForNamespace(ctx, accountNamespace)
 	}
 
 	var account Account
@@ -71,24 +81,9 @@ func (s *service) GetAccount(ctx context.Context, accountKey client.ObjectKey) (
 }
 
 func (s *service) GetAccountForNamespace(ctx context.Context, namespace string) (*Account, error) {
-	var ns corev1.Namespace
-	err := s.client.Get(ctx, client.ObjectKey{Name: namespace}, &ns)
+	accountName, accountNamespace, err := s.getAccountOwnerAndNamespaceForNamespace(ctx, namespace)
 	if err != nil {
 		return nil, err
-	}
-
-	if ns.Labels == nil {
-		return nil, errors.New("namespace does not have a label and therefore no connected account")
-	}
-
-	accountName, ok := ns.Labels[NamespaceAccountOwnerLabel]
-	if !ok || accountName == "" {
-		return nil, errors.New("namespace does not have an account-owner label and therefore no connected account")
-	}
-
-	accountNamespace, ok := ns.Labels[NamespaceAccountOwnerNamespaceLabel]
-	if !ok || accountNamespace == "" {
-		return nil, errors.New("namespace does not have an account-owner-namespace label and therefore no connected account")
 	}
 
 	var account Account
