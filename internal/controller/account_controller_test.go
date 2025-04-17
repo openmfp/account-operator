d54abb3347310d76351b50b4b24188c42cdd8bd2
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2025-04-17 08:35:27 +0200

    test: adjust unittest

--- internal/controller/account_controller_test.go
+++ internal/controller/account_controller_test.go
@@ -164,7 +164,7 @@ func (suite *AccountTestSuite) TestAddingFinalizer() {
 		return err == nil && createdAccount.Finalizers != nil
 	}, defaultTestTimeout, defaultTickInterval)
 
-	suite.Equal([]string{"account.core.openmfp.org/finalizer" /* , "account.core.openmfp.org/fga" */}, createdAccount.ObjectMeta.Finalizers)
+	suite.Equal([]string{"account.core.openmfp.org/finalizer", "account.core.openmfp.org/info"}, createdAccount.ObjectMeta.Finalizers)
 }
 
 func (suite *AccountTestSuite) TestWorkspaceCreation() {
